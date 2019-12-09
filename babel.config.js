7aadbf72a99aad9e097ff932ee03cf90d3714fd4
Corey Farrell <git@cfware.com>
2019-12-08 23:30:27 -0500

    chore: Add workaround for tap / import-jsx issue.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,13 @@
 'use strict';
 
 module.exports = {
-	plugins: [
-		'@babel/plugin-transform-modules-commonjs',
-		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-proposal-optional-catch-binding'
-	]
+	// Work around https://github.com/vadimdemedes/import-jsx/pull/8
+	overrides: [{
+		test: ['esm/*.js'],
+		plugins: [
+			'@babel/plugin-transform-modules-commonjs',
+			'@babel/plugin-proposal-class-properties',
+			'@babel/plugin-proposal-optional-catch-binding'
+		]
+	}]
 };
